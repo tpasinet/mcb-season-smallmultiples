63dfd0c1b47c09bfc01a3fdd9477b2288df1014a
tpasinet <tpasinet@users.noreply.github.com>
2017-03-12 17:57:29 -0400

    Renamed the main file, since it is more general than one team in one season.

--- wvu_season_smallmultiples.r
+++
@@ -1,6 +0,0 @@
-library(rvest)
-wvuSchedulePage <- read_html("http://www.espn.com/mens-college-basketball/team/schedule/_/id/277")
-wvuScheduleTable <- wvuSchedulePage %>%
-  html_nodes("table") %>%
-  .[[1]] %>%
-   html_table()
