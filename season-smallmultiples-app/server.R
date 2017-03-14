1c8408da555c6ebdeb621b4ac68444f4f3b6a63d
tpasinet <tpasinet@users.noreply.github.com>
2017-03-14 19:37:53 -0400

    Turned the logic for fixing axes into a function and call the function in each of the plots.

--- season-smallmultiples-app/server.R
+++ season-smallmultiples-app/server.R
@@ -4,54 +4,50 @@ library(ggplot2)
 
 load(file ="data/season_pbp.Rda")
 
+fix_axes <- function(fix_axis_x, fix_axis_y) {
+  
+  if(fix_axis_x == FALSE & fix_axis_y == FALSE){
+    "free"
+  } else if(fix_axis_x == TRUE & fix_axis_y == TRUE){
+    "fixed"
+  } else if(fix_axis_x == TRUE & fix_axis_y == FALSE){
+    "free_y"
+  } else if(fix_axis_x == FALSE & fix_axis_y == TRUE){
+    "free_x"
+  } else {
+    "error"
+  }
+  
+}
+
 shinyServer(function(input, output) {
    
   output$scoresPlot <- renderPlot({
     
-    fix_score_scales <- if(input$fix_score_axis_x == FALSE & input$fix_score_axis_y == FALSE){
-      "free"
-    } else if(input$fix_score_axis_x == TRUE & input$fix_score_axis_y == TRUE){
-      "fixed"
-    } else if(input$fix_score_axis_x == TRUE & input$fix_score_axis_y == FALSE){
-      "free_y"
-    } else if(input$fix_score_axis_x == FALSE & input$fix_score_axis_y == TRUE){
-      "free_x"
-    } else {
-      "error"
-    }
+    axes_fixed <- fix_axes(input$fix_score_axis_x, input$fix_score_axis_y)
     
     ggplot(season_pbp, aes(game_time_min, score_team_cumm)) +
       geom_line(color = "blue") +
       geom_line(aes(y = score_opponent_cumm), color = "gray") +
       geom_vline(xintercept = 20) +
       geom_vline(xintercept = 40) +
-      facet_wrap(~ game_title_score, scales = fix_score_scales) +
+      facet_wrap(~ game_title_score, scales = axes_fixed) +
       theme_bw() +
       ylab("Cumulative Points") +
-      xlab("Game Timein Minutes")
+      xlab("Game Time in Minutes")
     
   })
   
   output$marginPlot <- renderPlot({
     
-    fix_margin_scales <- if(input$fix_margin_axis_x == FALSE & input$fix_margin_axis_y == FALSE){
-      "free"
-    } else if(input$fix_margin_axis_x == TRUE & input$fix_margin_axis_y == TRUE){
-      "fixed"
-    } else if(input$fix_margin_axis_x == TRUE & input$fix_margin_axis_y == FALSE){
-      "free_y"
-    } else if(input$fix_margin_axis_x == FALSE & input$fix_margin_axis_y == TRUE){
-      "free_x"
-    } else {
-      "error"
-    }
+    axes_fixed <- fix_axes(input$fix_margin_axis_x, input$fix_margin_axis_y)
     
     ggplot(season_pbp, aes(game_time_min, score_team_diff)) +
       geom_line(color = "blue") +
       geom_hline(yintercept = 0, color = "gray") +
       geom_vline(xintercept = 20) +
       geom_vline(xintercept = 40) +
-      facet_wrap(~ game_title_margin, scales = fix_margin_scales) +
+      facet_wrap(~ game_title_margin, scales = axes_fixed) +
       theme_bw() +
       ylab("Score Margin") +
       xlab("Game Time in Minutes")
