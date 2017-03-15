22d78c74f1f057079912a00f9c7c7b8ddcb55878
tpasinet <tpasinet@users.noreply.github.com>
2017-03-15 03:27:39 -0400

    Added extensive comments and changed read and write paths of files.

--- season-smallmultiples-app/scripts/extract_espn_mcb_season_pbp.R
+++ season-smallmultiples-app/scripts/extract_espn_mcb_season_pbp.R
@@ -1,3 +1,9 @@
+# Scrape play-by-play (pbp) data from ESPN NCAA Men's Basketball game web pages.
+# For a given team in a givn season, retrive the team's schedule from their scheduale web page.
+# Extract all the game IDs from the schedule data.
+# Use each game ID to retrive the PBP data for that game.
+# Save the schedule and PBP data for further transformations.
+
 # Load libraries
 library(rvest)
 library(stringr)
@@ -9,40 +15,45 @@ library(lubridate)
 team_id <- "277"
 season_year <- "2017"
 
-# Define functins
+# Define functions.
+
+# Given a team ID and season year, retrive the team's schedule for that season.
 extract_season_schedule <- function(team_id, season_year) {
+  
+  # Set the base url for all schedulle web pages.
   schedule_base_url <- "http://www.espn.com/mens-college-basketball/team/schedule/_/id/"
   
+  # Extract the schedule table node from the schedule page html.
   season_schedule <- read_html(paste(schedule_base_url, team_id, "/year/",season_year, sep = "")) %>%
     html_node(xpath = "//div[@id = 'showschedule']//*//table")
   
-  # Get row classes
+  # Get row classes for use as row filters to select actual games
   row_classes <- season_schedule %>%
     html_nodes(xpath = "//tr") %>%
     html_attr("class")
   
-  # Get schedule data as a table
+  # Get schedule data as a table.
   season_schedule_table <- season_schedule %>%
     html_table()
   
-  # Get column headers
+  # Get column headers from the schedule table. 
   column_headers <- season_schedule_table %>%
     slice(which(row_classes == "colhead")) %>%
     as.character() %>%
     gsub("/[A-Z]+$", "", .) %>%
     tolower()
   
-  #Set variable names
+  #Set variable names using the colun headers extracted from the schedule table.
   colnames(season_schedule_table) <- column_headers
   
-  # Get game IDs from score link
+  # Get game IDs from the score link href.
   game_ids <- season_schedule %>%
     html_nodes(xpath = "//table//tr//td//*[contains(@class, 'score')]//a") %>%
     html_attr("href") %>%
     str_extract("/gameId/\\d+$") %>%
     str_replace("/gameId/","")
   
-  # Add row classes, filter out not result rows, add team, season year, game ids, and row class
+  # Add row classes, filter out not result rows, add team, season year, game ids, and row class.
   season_schedule_table <- season_schedule_table %>%
     mutate(row_class = row_classes) %>%
     filter(grepl("^[a-z]+row ", row_class)) %>%
@@ -55,15 +66,22 @@ extract_season_schedule <- function(team_id, season_year) {
   season_schedule_table
 }
 
+# For a given game, extract the game's PBP data from the game's PBP web page.
 extract_game_pbp <- function(game_id) {
+  
+  # Set the base url for all game PBP web pages.
   game_pbp_base_url <- "http://www.espn.com/mens-college-basketball/playbyplay?gameId="
   
+  # Extract the PBP web page html.
   game_pbp_page <- read_html(paste(game_pbp_base_url, game_id, sep = ""))
   
+  # Extract the quarters values from the divs for each quarter's PBP section. In this case, quarters 1 and 2 represent periods 1 and 2.
+  # Any quarter over 2 is an overtime period.
   game_quarters <- game_pbp_page %>%
     html_nodes(xpath = "//div[starts-with(@id, 'gp-quarter-')]") %>%
     html_attr("id")
   
+  # Extract the PBP data from each quarter's PBP table and, union the resulting rows, and append the game ID in a new column.
   game_pbp_table <- lapply(game_quarters, extract_quarter_pbp, game_pbp_page = game_pbp_page) %>%
     bind_rows() %>%
     mutate(game_id = game_id)
@@ -71,14 +89,19 @@ extract_game_pbp <- function(game_id) {
   game_pbp_table
 }
 
+# Given a game's PBP page and a quarter, extract the quarter's PBP data.
 extract_quarter_pbp <- function(game_pbp_page, game_quarter) {
+  
+  # Extract the quarter's PBP html table.
   quarter_pbp <- game_pbp_page %>%
     html_nodes(xpath = paste("//div[@id = '", game_quarter,"']//table", sep = ""))
   
+  # Extract score changes to use as a filter for PBP rows that represent a change in the score of either team.
   score_changes <- quarter_pbp %>%
     html_nodes(xpath = "*//td[starts-with(@class, 'combined-score')]") %>%
     html_attr("class") 
   
+  # Extract the quarter's PBP data and append useful metadata as new columns.
   quarter_pbp_table <- quarter_pbp %>%
     html_table(fill = TRUE) %>%
     .[[1]] %>%
@@ -90,13 +113,14 @@ extract_quarter_pbp <- function(game_pbp_page, game_quarter) {
   quarter_pbp_table
 }
 
-# Get the team's season schedule
+# Get the team's season schedule.
 season_schedule_extracted <- extract_season_schedule(team_id, season_year) %>%
   mutate(extract_timestamp = Sys.time())
 
-# Get the team's season's plays
+# Get the team's season's plays.
 season_pbp_extracted <- lapply(season_schedule_extracted$game_id, extract_game_pbp) %>%
   bind_rows() %>%
   mutate(extract_timestamp = Sys.time())
 
-save(season_schedule_extracted, season_pbp_extracted, file="./data/season_data_extracted.RData")
+# Save the extracted data.
+save(season_schedule_extracted, season_pbp_extracted, file="season-smallmultiples-app/data/season_data_extracted.RData")
--- season-smallmultiples-app/scripts/transform_extracted_data.R
+++ season-smallmultiples-app/scripts/transform_extracted_data.R
@@ -1,19 +1,30 @@
-# Load libraries
-library(stringr)
+# Given a R RData file containing NCAA Men's Basketball schedule and game play-by-play (PBP) data extracted fro ESPN game web pages,
+# transform the data for use in graphs of cumulative scores and points margin by game by second. 
+
+# Load libraries.
 library(dplyr)
+library(stringr)
 library(lubridate)
 
-load(file = "../data/season_data_extracted.RData")
+# Load the etracted data.
+load(file = "season-smallmultiples-app/data/season_data_extracted.RData")
 
+# Transform the etracted schedule data for use in the transformation of the game PBP data
 season_schedule <- season_schedule_extracted %>%
+  # Construct a POSIX date from date parts stored as text for use in constructing a play timestamp.
+  # Dates in the table do not have year parts. Dervie the year given the month part and season year.
   mutate(season_year_start = as.character(as.integer(season_year) - 1)) %>%
   mutate(game_date = ifelse(grepl("(Nov|Dec)", date), season_year_start, season_year)) %>%
   mutate(game_date = paste(date, game_date, sep = ', ')) %>%
   mutate(game_date = parse_date_time(game_date, "%a, %b! %d!, %Y!")) %>%
+  # Determine if the game was a home game for the team whose schedule is being transformed.
   mutate(is_home_game = grepl("^vs", opponent)) %>%
+  # Condition the opponents name by removing unnecessary characters.
   mutate(opponent = gsub("(vs|@|#\\d+)", "", opponent, perl=TRUE)) %>%
   mutate(opponent = gsub("\\*", "", opponent, perl=TRUE)) %>%
+  # Determine if the game was a win for the team whose schedule is being transformed.
   mutate(is_win = grepl("^W", result, perl = TRUE)) %>%
+  # Extract the score result and split into a score for the team and a score for the opponent.
   mutate(result = str_extract(result, "\\d+-\\d+")) %>%
   mutate(score_win = str_extract(result, "^\\d+")) %>%
   mutate(score_win = as.numeric(score_win)) %>%
@@ -21,42 +32,59 @@ season_schedule <- season_schedule_extracted %>%
   mutate(score_lose = as.numeric(score_lose)) %>%
   mutate(score_team_total = if_else(is_win, score_win, score_lose)) %>%
   mutate(score_opponent_total = if_else(is_win, score_lose, score_win)) %>%
+  # Extract and append the opponent's team id.
   mutate(opponent_id = str_extract(row_class, "\\d+$")) %>%
   mutate(opponent_id = as.numeric(opponent_id)) %>%
-  select(team_id, season_year, game_id, game_index, game_date, opponent_id, opponent, is_home_game, is_win, score_team_total, score_opponent_total)
+  # Select only columns of actual interest.
+  select(team_id, season_year, game_id, game_index, game_date, opponent_id, opponent, is_home_game, is_win,
+         score_team_total, score_opponent_total)
 
+# Given transformed schedule data, transform the extracted PBP data
 season_pbp <- season_pbp_extracted %>%
+  # Filter out any plays that did not result in a score change for either team.
   filter(play_score_change != 'combined-score no-change') %>%
+  # Join data from the transformed schedule data.
   inner_join(season_schedule, by = "game_id") %>%
-  mutate(game_score_cumm_home = str_extract(SCORE, "\\d+$")) %>%
-  mutate(game_score_cumm_away = str_extract(SCORE, "^\\d+")) %>%
+  # Set the period of the play.
   mutate(game_period = str_extract(game_quarter, "\\d+$")) %>%
   mutate(game_period = as.numeric(game_period)) %>%
+  # Derive the team and opponent scores for each play.
+  mutate(game_score_cumm_home = str_extract(SCORE, "\\d+$")) %>%
+  mutate(game_score_cumm_away = str_extract(SCORE, "^\\d+")) %>%
   mutate(score_team_cumm = ifelse(is_home_game, game_score_cumm_home, game_score_cumm_away)) %>%
   mutate(score_team_cumm = as.numeric(score_team_cumm)) %>%
   mutate(score_opponent_cumm = if_else(is_home_game, game_score_cumm_away, game_score_cumm_home)) %>%
   mutate(score_opponent_cumm = as.numeric(score_opponent_cumm)) %>%
+  # Derive a combined score as an alternative method for indicating score changes. May depricate in the future.
   mutate(score_combined_total = score_team_cumm + score_opponent_cumm) %>%
+  # Derive the points margin from the perspective of the team.
   mutate(score_team_diff = score_team_cumm - score_opponent_cumm) %>%
+  # Derive a play timestamp by converting the time in period to a time in game and appending the result to the game date. 
   mutate(period_time_sec = ifelse(game_period <= 2, 1200, 300) - seconds(ms(time))) %>%
   mutate(game_time_sec = seconds(ifelse(game_period <= 2, (game_period -1) * 1200, 2400 + ((game_period - 2)  * 300)))) %>%
   mutate(game_time_sec = game_time_sec + period_time_sec) %>%
   mutate(play_timestamp = as.POSIXct(game_date + game_time_sec)) %>%
   mutate(play_timestamp = as_datetime(play_timestamp)) %>%
+  # Derive other usefule time indexes for the play.
   mutate(period_time_sec = second(period_time_sec)) %>%
   mutate(game_time_sec = second(game_time_sec)) %>%
   mutate(game_time_min = game_time_sec / 60) %>%
+  # Derive tiltles for use in facet labels. May depricate in favor of facet labeling from schedule data.
   mutate(game_score_result = paste(as.character(score_team_total), "-", as.character(score_opponent_total), sep = "")) %>%
   mutate(game_title_score = paste(game_date, ifelse(is_home_game, "vs", "@"), opponent, ifelse(is_win, "W", "L"), game_score_result)) %>%
   mutate(game_score_team_margin = score_team_total - score_opponent_total) %>%
   mutate(game_score_team_margin = paste(ifelse(game_score_team_margin > 0, "+", ""), as.character(game_score_team_margin), sep = "")) %>%
   mutate(game_title_margin = paste(game_date, ifelse(is_home_game, "vs", "@"), opponent, ifelse(is_win, "W", "L"), game_score_team_margin)) %>%
+  # Derive a play ID that uniquely identifies the play using metadata.
   mutate(play_id = paste(game_id, str_pad(as.character(game_period), 2, "left", "0"), str_pad(as.character(play_period_index), 3, "left", "0"), sep = "")) %>%
+  # Give the PLAY column a more descriptive name.
   rename(play_description = PLAY) %>%
+  # Select only columns of interest or use.
   select(extract_timestamp, team_id, season_year, game_id, game_title_score, game_title_margin, game_date, opponent_id,
          opponent, is_home_game, is_win, score_team_total, score_opponent_total, play_id, play_timestamp, game_period, period_time_sec,
          game_time_sec, game_time_min, play_description, score_team_cumm, score_opponent_cumm, score_combined_total, score_team_diff)
 
-save(season_schedule, file="../data/season_scheduled.Rda")
-save(season_pbp, file="../data/season_pbp.Rda")
+# Save the transformed data.
+save(season_schedule, file="season-smallmultiples-app/data/season_schedule.Rda")
+save(season_pbp, file="season-smallmultiples-app/data/season_pbp.Rda")
 
